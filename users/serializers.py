6217557f717bf350509ca2bab039cc1de5bdd8fa
Adam Gulczynski <adamg@example.com>
2023-03-28 00:14:51 +0200

    #5 completed. Token authentication.

--- users/serializers.py
+++ users/serializers.py
@@ -4,7 +4,7 @@ Serializers for the users API.
 from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import gettext as _
 
-from rest_framework import serializers, exceptions
+from rest_framework import serializers
 
 from users.models import UserProfile
 
@@ -60,3 +60,28 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
         return profile
 
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        required=True
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user'] = user
+        return attrs
